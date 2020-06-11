5539cd6c90d843c0b5466e8efa05e67183037aef
MarceloVasquez662 <marcelovasquez662@gmail.com>
2020-06-10 20:09:34 -0400

    Updates

---
+++ precache-manifest.83547224caf470d4834b250a83f7605c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9d8671d3f0dde6a677455abf9e2bbb",
+    "url": "/React_SPA/index.html"
+  },
+  {
+    "revision": "107d182d21e195107096",
+    "url": "/React_SPA/static/css/main.8d89dd91.chunk.css"
+  },
+  {
+    "revision": "d1a142d69ffd690e2989",
+    "url": "/React_SPA/static/js/2.ee025c67.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_SPA/static/js/2.ee025c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107d182d21e195107096",
+    "url": "/React_SPA/static/js/main.0b15a750.chunk.js"
+  },
+  {
+    "revision": "e45148d7c00ed3d0008b",
+    "url": "/React_SPA/static/js/runtime-main.4376b407.js"
+  },
+  {
+    "revision": "290d46d9acd351aafe51cc60f9bb3022",
+    "url": "/React_SPA/static/media/Acerca.290d46d9.svg"
+  },
+  {
+    "revision": "e6c4e27c666d43a2dcbfbe17fbd50cf4",
+    "url": "/React_SPA/static/media/fondoBanner.e6c4e27c.jpg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/React_SPA/static/media/logo.4009eef3.svg"
+  }
+]);
